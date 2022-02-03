dcd64e6ca33c537a07cd5f2571b16c9495beff0d
Austin Cory Bart <acbart@vt.edu>
2022-02-03 14:10:55 -0500

    Allow one or more instances of the Hello World text

--- src/text.test.tsx
+++ src/text.test.tsx
@@ -5,10 +5,15 @@ import App from "./App";
 test("renders the text 'Hello World' somewhere", () => {
     render(<App />);
 <<<<<<< HEAD
+<<<<<<< HEAD
     const texts = screen.getAllByText(/Hello World/);
     expect(texts.length).toBeGreaterThanOrEqual(1);
 =======
     const text = screen.getByText(/Hello World/);
     expect(text).toBeInTheDocument();
 >>>>>>> a7dee05 (Rename text.Test.tsx to text.test.tsx)
+=======
+    const texts = screen.getAllByText(/Hello World/);
+    expect(texts.length).toBeGreaterThanOrEqual(1);
+>>>>>>> e6b1dab (Allow one or more instances of the Hello World text)
 });
