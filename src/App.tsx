761bc6a89f75df1207c2a517ebe183b9d1fa179c
Austin Cory Bart <acbart@vt.edu>
2022-02-09 18:10:22 -0500

    Basic starter files for components

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,30 @@
 import React from "react";
 import "./App.css";
-import {Button} from 'react-bootstrap';
+import { Counter } from "./components/Counter";
+import { ChangeType } from "./components/ChangeType";
+import { RevealAnswer } from "./components/RevealAnswer";
+import { StartAttempt } from "./components/StartAttempt";
+import { TwoDice } from "./components/TwoDice";
+import { CycleHoliday } from "./components/CycleHoliday";
 
 function App(): React.JSX.Element {
-    let fruits: string[] = ["one", "two", "three"];
-
     return (
         <div className="App">
-            <header className="App-header" style={{ backgroundColor: "red" }}
-            >
-                UD CISC275 with React Hooks and TypeScript Nathaniel Black Hello World
-                <h1>New Header</h1>;
+            <header className="App-header">
+                UD CISC275 with React Hooks and TypeScript
             </header>
-
-            <img src="https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_272x92dp.png" alt="Alt text for my image" />
-           
-        {fruits.map((fruit, index) => <div key={index}>{fruit}</div>)}
-        <Button onClick={() => { console.log("Hello World!") }}>Log Hello World</Button>     
-
-        <ul>
-            <li>First</li>
-            <li>Second</li>
-            <li>Third</li>
-        </ul>
-
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
+            <hr />
+            <Counter></Counter>
+            <hr />
+            <RevealAnswer></RevealAnswer>
+            <hr />
+            <StartAttempt></StartAttempt>
+            <hr />
+            <TwoDice></TwoDice>
+            <hr />
+            <ChangeType></ChangeType>
+            <hr />
+            <CycleHoliday></CycleHoliday>
         </div>
     );
 }
