6077a75b6269a5dc72357f27039491fd98d7dea6
Austin Cory Bart <acbart@vt.edu>
2022-02-24 10:48:53 -0500

    Another subtask, ChooseTeam

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ import { DoubleHalf } from "./bad-components/DoubleHalf";
 import { ColoredBox } from "./bad-components/ColoredBox";
 import { ShoveBox } from "./bad-components/ShoveBox";
 import { TrackingNumbers } from "./bad-components/TrackingNumbers";
+import { ChooseTeam } from "./bad-components/ChooseTeam";
 
 function App(): React.JSX.Element {
     return (
@@ -20,6 +21,8 @@ function App(): React.JSX.Element {
             </header>
             <hr></hr>
             {/* <DoubleHalf></DoubleHalf> */}
+            <hr></hr>
+            <ChooseTeam></ChooseTeam>
             <hr></hr>
             <ColoredBox></ColoredBox>
             <hr></hr>
