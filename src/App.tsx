a4c445839d0835979983c92e35724d888cd9962e
Austin Cory Bart <acbart@vt.edu>
2022-02-24 13:01:04 -0500

    Ugh this component is stupid, let's just forget about it for now

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,6 @@ import { Counter } from "./components/Counter";
 import { DoubleHalf } from "./bad-components/DoubleHalf";
 import { ColoredBox } from "./bad-components/ColoredBox";
 import { ShoveBox } from "./bad-components/ShoveBox";
-import { TrackingNumbers } from "./bad-components/TrackingNumbers";
 import { ChooseTeam } from "./bad-components/ChooseTeam";
 
 function App(): React.JSX.Element {
@@ -27,8 +26,6 @@ function App(): React.JSX.Element {
             <ColoredBox></ColoredBox>
             <hr></hr>
             <ShoveBox></ShoveBox>
-            <hr></hr>
-            <TrackingNumbers></TrackingNumbers>
             <hr></hr>
             <Counter></Counter>
             <hr />
