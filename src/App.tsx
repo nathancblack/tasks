a7b10cb982a5cce15422740c928d159e817362b3
Nathan Black <nate@wifi-roaming-128-4-114-138.wifi.udel.edu>
2026-02-09 12:57:11 -0500

    Completed Task 2

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript Nathaniel Black
+                UD CISC275 with React Hooks and TypeScript Nathaniel Black Hello World
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
