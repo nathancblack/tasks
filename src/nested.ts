a0e65975383d07c5c9d8fc05462919133932503d
Nathaniel Black <ncb@udel.edu>
2026-03-04 17:32:31 -0500

    toCSV function passes unit test

--- src/nested.ts
+++ src/nested.ts
@@ -81,7 +81,7 @@ id,name,options,points,published
  * Check the unit tests for more examples!
  */
 export function toCSV(questions: Question[]): string {
-    return `id, name, options, points, published\n${questions.map((question: Question): string => 
+    return `id,name,options,points,published\n${questions.map((question: Question): string => 
     `${question.id},${question.name},${question.options.length},${question.points},${question.published}`).join("\n")}`;
 }
 
