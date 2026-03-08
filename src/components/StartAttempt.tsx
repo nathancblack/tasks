858f2db5ec4e44bf842ce084b8f0a20907106c92
Nathaniel Black <ncb@udel.edu>
2026-03-08 16:51:08 -0400

    Complete ChangeType and StartAttempt

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -1,6 +1,30 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
-export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+export function StartAttempt(): React.JSX.Element {
+    const [attempts, setAttempts] = useState<number>(4);
+    const [inProgress, setInProgress] = useState<boolean>(false);
+      
+    function startQuiz(): void {
+        setInProgress(true);
+        setAttempts(attempts - 1)
+    }
+    
+    function stopQuiz(): void {
+      setInProgress(false);
+    }
+
+    
+    // Start Quiz -> inProgress == true and attempts -= 1
+    // Stop Quiz -> inProgress == false
+    // Mulligan -> attempts += 1
+    // inProgess === true -> Start Quiz and Mulligan === disabled
+    // inProgress === false -> Stop Quiz === disabled
+    //
+    return <div>
+      <div>{attempts}</div>
+      <Button disabled={inProgress || attempts === 0 } onClick={startQuiz}>Start Quiz</Button>
+      <Button disabled={!inProgress} onClick={stopQuiz}>Stop Quiz</Button>
+      <Button disabled={inProgress} onClick={() => {setAttempts(attempts + 1)}}>Mulligan</Button>
+    </div>
 }
